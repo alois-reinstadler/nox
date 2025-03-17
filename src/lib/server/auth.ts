82c5b11e20e20d0313754d8236ca5d7429f176c4
Alois R <alois-reinstadler@proton.me>
2025-03-18 00:56:46 +0100

    add nodejs compat

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -31,7 +31,13 @@ export async function validateSessionToken(token: string) {
 	const [result] = await db
 		.select({
 			// Adjust user table here to tweak returned data
-			user: { id: table.user.id, username: table.user.username },
+			user: {
+				id: table.user.id,
+				email: table.user.email,
+				firstName: table.user.firstName,
+				lastName: table.user.lastName,
+				avatar: table.user.avatar
+			},
 			session: table.session
 		})
 		.from(table.session)
--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -2,14 +2,25 @@ import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
 
 export const user = sqliteTable('user', {
 	id: text('id').primaryKey(),
-	age: integer('age')
+
+	email: text('email').notNull().unique(),
+	password: text('password').notNull(),
+
+	firstName: text('first_name'),
+	lastName: text('last_name'),
+	avatar: text('avatar'),
+
+	updatedAt: integer('updated_at', { mode: 'timestamp' }),
+	createdAt: integer('created_at', { mode: 'timestamp' }).notNull()
 });
 
 export const session = sqliteTable('session', {
 	id: text('id').primaryKey(),
+
 	userId: text('user_id')
 		.notNull()
 		.references(() => user.id),
+
 	expiresAt: integer('expires_at', { mode: 'timestamp' }).notNull()
 });
 
