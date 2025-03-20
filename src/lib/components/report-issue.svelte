91a0b743ef8c4b7e93c0e1c64f4dd290cbdd2901
Alois R <alois-reinstadler@proton.me>
2025-03-20 23:32:41 +0100

    misc

---
+++ src/lib/components/report-issue.svelte
@@ -0,0 +1,103 @@
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card/index.js';
+	import * as Select from '$lib/components/ui/select/index.js';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import { Label } from '$lib/components/ui/label/index.js';
+	import { Textarea } from '$lib/components/ui/textarea/index.js';
+
+	const areas = [
+		{
+			value: 'team',
+			label: 'Team'
+		},
+		{
+			value: 'billing',
+			label: 'Billing'
+		},
+		{
+			value: 'account',
+			label: 'Account'
+		},
+		{
+			value: 'deployments',
+			label: 'Deployments'
+		},
+		{
+			value: 'support',
+			label: 'Support'
+		}
+	];
+
+	const securityLevels = [
+		{
+			value: '1',
+			label: 'Severity 1 (Highest)'
+		},
+		{
+			value: '2',
+			label: 'Severity 2'
+		},
+		{
+			value: '3',
+			label: 'Severity 3'
+		},
+		{
+			value: '4',
+			label: 'Severity 4 (Lowest)'
+		}
+	];
+
+	let areaValue = $state(areas[1].value);
+	let securityValue = $state(securityLevels[1].value);
+</script>
+
+<Card.Root class="max-w-md">
+	<Card.Header>
+		<Card.Title>Report an issue</Card.Title>
+		<Card.Description>What area are you having problems with?</Card.Description>
+	</Card.Header>
+	<Card.Content class="grid gap-6">
+		<div class="grid grid-cols-2 gap-4">
+			<div class="grid gap-2">
+				<Label for="area">Area</Label>
+				<Select.Root type="single" bind:value={areaValue}>
+					<Select.Trigger id="area">Select</Select.Trigger>
+					<Select.Content>
+						{#each areas as area}
+							<Select.Item value={area.value} label={area.label}>{area.label}</Select.Item>
+						{/each}
+					</Select.Content>
+				</Select.Root>
+			</div>
+			<div class="grid gap-2">
+				<Label for="security-level">Security Level</Label>
+				<Select.Root type="single" bind:value={securityValue}>
+					<Select.Trigger id="security-level" class="line-clamp-1 flex w-[160px] truncate">
+						Select level
+					</Select.Trigger>
+					<Select.Content>
+						{#each securityLevels as level}
+							<Select.Item value={level.value} label={level.label}>{level.label}</Select.Item>
+						{/each}
+					</Select.Content>
+				</Select.Root>
+			</div>
+		</div>
+		<div class="grid gap-2">
+			<Label for="subject">Subject</Label>
+			<Input id="subject" placeholder="I need help with..." />
+		</div>
+		<div class="grid gap-2">
+			<Label for="description">Description</Label>
+			<Textarea
+				id="description"
+				placeholder="Please include all information relevant to your issue."
+			/>
+		</div>
+	</Card.Content>
+	<Card.Footer class="justify-between space-x-2">
+		<Button variant="ghost">Cancel</Button>
+		<Button>Submit</Button>
+	</Card.Footer>
+</Card.Root>
--- src/routes/(app)/app-header.svelte
+++ src/routes/(app)/app-header.svelte
@@ -13,33 +13,35 @@
 <header
 	class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-in-out group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12"
 >
-	<div class="container flex items-center gap-2">
-		<Sidebar.Trigger class="-ml-1" />
-		<Separator orientation="vertical" class="mx-1 h-4" />
+	<div class="container flex w-full items-center justify-between">
+		<div class="container flex items-center gap-2">
+			<Sidebar.Trigger class="-ml-1" />
+			<Separator orientation="vertical" class="mx-1 h-4" />
 
-		<Breadcrumbs url={page.url} routeId={page.route.id} pageData={page.data} skipRoutesWithNoPage>
-			{#snippet children({ crumbs })}
-				<Breadcrumb.Root>
-					<Breadcrumb.List class="relative">
-						<Breadcrumb.Item class="hidden md:block">
-							<Breadcrumb.Link class="h-7 w-7" href="/">
-								{#snippet child({ props }: Record<'props', Record<string, any>>)}
-									<Button variant="ghost" size="icon" href="/" {...props}>
-										<Home />
-										<span class="sr-only">Home</span>
-									</Button>
-								{/snippet}
-							</Breadcrumb.Link>
-						</Breadcrumb.Item>
-						{#each crumbs as c, i (c.url)}
-							<Breadcrumb.Separator />
-							<Breadcrumb.Item>
-								<Breadcrumb.Link href={c.url}>{c.title}</Breadcrumb.Link>
+			<Breadcrumbs url={page.url} routeId={page.route.id} pageData={page.data} skipRoutesWithNoPage>
+				{#snippet children({ crumbs })}
+					<Breadcrumb.Root>
+						<Breadcrumb.List class="relative">
+							<Breadcrumb.Item class="hidden md:block">
+								<Breadcrumb.Link class="h-7 w-7" href="/">
+									{#snippet child({ props }: Record<'props', Record<string, any>>)}
+										<Button variant="ghost" size="icon" href="/" {...props}>
+											<Home />
+											<span class="sr-only">Home</span>
+										</Button>
+									{/snippet}
+								</Breadcrumb.Link>
 							</Breadcrumb.Item>
-						{/each}
-					</Breadcrumb.List>
-				</Breadcrumb.Root>
-			{/snippet}
-		</Breadcrumbs>
+							{#each crumbs as c, i (c.url)}
+								<Breadcrumb.Separator />
+								<Breadcrumb.Item>
+									<Breadcrumb.Link href={c.url}>{c.title}</Breadcrumb.Link>
+								</Breadcrumb.Item>
+							{/each}
+						</Breadcrumb.List>
+					</Breadcrumb.Root>
+				{/snippet}
+			</Breadcrumbs>
+		</div>
 	</div>
 </header>
